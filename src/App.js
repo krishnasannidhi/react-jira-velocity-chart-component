57b4ead81d18435b7d7e2b6b568c523b639d4406
krishna <krishna.sannidhi@gmail.com>
2024-06-15 18:54:02 -0500

    added toggle button and barchart skelton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import BarChart from "./components/BarChart";
 
 function App() {
+  const [showChart, setShowChart] = useState(false);
   return (
-    <div>
+    <div className="container">
       <h1 className="title">JIRA Velocity Chart Component</h1>
+      <button onClick={() => setShowChart(!showChart)}>Toggle Chart</button>
+      {showChart && <BarChart />}
     </div>
   );
 }
---
+++ src/data/chart.js
@@ -0,0 +1,14 @@
+export const CHART_DATA = [
+  { id: "dep-1", name: "Legal", ticketCount: 32, color: "#3F888F" },
+  { id: "dep-2", name: "Sales", ticketCount: 20, color: "#FFA420" },
+
+  { id: "dep-3", name: "Engineering", ticketCount: 60, color: "#3F888F" },
+
+  { id: "dep-4", name: "Manfacturing", ticketCount: 5, color: "#1D1E33" },
+
+  { id: "dep-5", name: "Maintenance", ticketCount: 14, color: "#E1CC4F" },
+
+  { id: "dep-6", name: "Human Resourcing", ticketCount: 35, color: "#1D1E33" },
+
+  { id: "dep-7", name: "Events", ticketCount: 43, color: "#E1CC4F" },
+];
